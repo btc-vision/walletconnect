1355ae5a20dc3f3986b708d6377a18d40f7d1171
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-12-22 20:32:29 -0500

    Export supported wallets from index.ts
    Added export for all items from './wallets/supported-wallets.js' in the main index file to make supported wallets available to consumers of the package. Bumped package version to 1.9.15.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { useWalletConnect } from './hooks/WalletConnectHook';
 export * from './types';
 
 export { default as WalletConnectProvider } from './provider/WalletConnectProvider';
+export * from './wallets/supported-wallets.js'
